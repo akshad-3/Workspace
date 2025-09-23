c619c27e2d48b158fce531794ac4f0b273183ace
akshad-3 <akshadofficial3@gmail.com>
2025-09-23 17:16:33 +0530

    class method

---
+++ Class/Class_1.py
@@ -0,0 +1,10 @@
+class employe:
+    lang="python"
+    salary=1200000
+    
+    
+    def getinfo(self):
+        print(f"the lang is {self.lang}, & the salary is {self.salary}")
+
+mike=employe()
+mike.getinfo()
