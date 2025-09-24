7e9b2001a02af2ee3a9bfecb32bd19b86a06384f
akshad-3 <akshadofficial3@gmail.com>
2025-09-24 20:31:26 +0530

    Try & except concept

---
+++ Advanced/try.py
@@ -0,0 +1,19 @@
+try:
+    with open("text1.txt","r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
+
+try:
+    with open("text2.txt","r") as f:   # <-- no indent
+        print(f.read())
+except Exception as e:
+    print(e)
+    
+try:
+    with open("text3.txt","r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
+
+print("Code is executed")
