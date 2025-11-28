26cc4f8b8bf4860e68bec56e953b3c37066f3947
akshad-3 <akshadofficial3@gmail.com>
2025-11-29 03:50:18 +0530

    comment

--- Java/fileio.java
+++ Java/fileio.java
@@ -1,3 +1,22 @@
+import java.io.*;
+
 public class fileio {
-    
+    public static void main(String[] args) {
+        try {
+        BufferedWriter writer = new BufferedWriter(new FileWriter("O.txt"));
+        writer.write("I am writing this file after implemanting the FIle I/O.");
+        writer.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("O.txt"));
+            System.out.printf(reader.readLine());
+            reader.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }    
 }
