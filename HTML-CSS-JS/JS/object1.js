72f0afef0a04b91454202f431063efbcdb274d8b
akshad-3 <akshadofficial3@gmail.com>
2025-12-14 21:51:31 +0530

    This is the basic Objectr Litral

---
+++ HTML-CSS-JS/JS/object1.js
@@ -0,0 +1,12 @@
+/*So this is the basic object syntax that we use 
+    to creat a object.
+    IMPORTANT!!!!
+    class = car design
+    object = actual car
+
+    This is called a Obect Litaral!!
+*/
+let user ={
+    name="rahul",
+    age=30;
+}
