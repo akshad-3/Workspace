a98d06cffe266992e5e461e9661705db32b1b45f
akshad-3 <akshadofficial3@gmail.com>
2026-01-01 22:59:03 +0530

    comments

--- HTML-CSS-JS/JS/object1.js
+++ HTML-CSS-JS/JS/object1.js
@@ -1,3 +1,5 @@
+//we also can make a object using a contructor whcih use singleton
+//the singleton does not form by literal syntax
 /*So this is the basic object syntax that we use 
     to creat a object.
     IMPORTANT!!!!
