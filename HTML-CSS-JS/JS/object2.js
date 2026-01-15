3d635cf738b5b180fec047c20e29ec9dc1b46a9b
akshad-3 <akshadofficial3@gmail.com>
2026-01-15 15:58:44 +0530

    optinal chaning

--- HTML-CSS-JS/JS/object2.js
+++ HTML-CSS-JS/JS/object2.js
@@ -1,8 +1,19 @@
+// let user = {
+//     name : "akshadd",
+//     id:"233"
+// }
+// user.sayhi = function() {
+//     console.log("hello world");
+// }
+// user.sayhi();
 let user = {
-    name : "akshadd",
-    id:"233"
-}
-user.sayhi = function() {
-    console.log("hello world");
-}
-user.sayhi();
+  profile: {
+    address: {
+      city: "Mumbai"
+    }
+  }
+};
+
+console.log(user.profile?.address?.city);  // Mumbai
+console.log(user.profile.contact.phone); // undefined
+
