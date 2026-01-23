04ce581aabb38b6657d3bc6039a7aba443e03e08
akshad-3 <akshadofficial3@gmail.com>
2026-01-23 21:11:43 +0530

    Classes in JS (basics of Classes

---
+++ HTML-CSS-JS/JS/class1.js
@@ -0,0 +1,12 @@
+class class1 {
+    constructor (name){
+        this.yourname=name;
+    }
+    sayhi(){
+        console.log(this.yourname);
+    }
+
+}
+let user = new class1("akshad");
+user.sayhi();
+console.log(user.yourname);
