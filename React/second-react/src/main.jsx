4880e1aae13308cb5dcc1ac0b18a46d2c76bc3d3
akshad-3 <akshadofficial3@gmail.com>
2026-03-15 01:30:33 +0530

    the parent and child component thing

--- React/second-react/src/main.jsx
+++ React/second-react/src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {createRoot} from "react-dom/client"
+import reactLogo from "/Users/akshad/GitHub/Workspace/React/second-react/src/react.svg"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById('root'))
+function Hearder(){
+    return(
+        <header>
+            <img src={reactLogo} width="50px" alt="React logo" />
+        </header>
+    )
+}
+function Maincontent(){
+    return(
+        <main>
+            <h1>Fun facts about React!</h1>
+            <ul>
+                <li>Was first release in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
+function Footer(){
+    return(
+        <footer>
+            <small>this page is @reserver by this man here all the Rights</small>
+        </footer>
+    )
+}
+function Page() {
+    return (
+        <>
+        <Hearder />
+        <Maincontent />
+        <Footer />
+        </>
+    )
+}
+root.render(
+    <Page />
 )
+
