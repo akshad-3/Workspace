589de4239c56f9fa86bd7d047f9ead250e88e950
akshad-3 <akshadofficial3@gmail.com>
2026-03-23 19:17:47 +0530

    the travel journal complete

--- React/Travel-journal/src/component/Entry.jsx
+++ React/Travel-journal/src/component/Entry.jsx
@@ -1,16 +1,18 @@
+import ping from "../assets/PING.png"
+
 export default function Entry(props){
     return(
         <article className="journal-entry">
             <div>
-                <img className="main-image" src={props.img.src} alt={props.img.alt} />
+                <img className="main-image" src={props.places.img.src} alt={props.places.img.alt} />
             </div>
             <div className="info-container">
-                <img className="ping-image" src={props.pingImage} alt="ping" />
-                <span className="country" >{props.countryName}</span>
-                <a href={props.countryLocation}>view on Google map</a>
-                <h2 className="entry-title">{props.countryTitle}</h2>
-                <p className="entry-date">{props.countryDate}</p>
-                <p className="entry-txt">{props.countryDisciption}</p>
+                <img className="ping-image" src={ping} alt="ping" />
+                <span className="country" >{props.places.country}</span>
+                <a href={props.places.googleMapsLinks}>view on Google map</a>
+                <h2 className="entry-title">{props.places.title}</h2>
+                <p className="entry-date">{props.places.dates}</p>
+                <p className="entry-txt">{props.places.text}</p>
             </div>
 
         </article>
