d58c7333ebfe9d3a6ea5bd878587e048134ee597
akshad-3 <akshadofficial3@gmail.com>
2026-03-28 06:55:17 +0530

    added

--- React/contact-log/src/contact.jsx
+++ React/contact-log/src/contact.jsx
@@ -28,3 +28,30 @@ export default function Contact(props){
         </article>
     )
 }
+
+// export default function Contact({img,name,phone,email}){
+//     console.log()
+//     return(
+//         <article className="contact-card">
+//             <img
+//                 src={img}
+//                 alt="Photo of Mr. Whiskerson"
+//             />
+//             <h3>{name}</h3>
+//             <div className="info-group">
+//                 <img
+//                     src={phoneimage1}
+//                     alt="phone icon"
+//                 />
+//                 <p>{phone}</p>
+//             </div>
+//             <div className="info-group">
+//                 <img
+//                     src={mailimage1}
+//                     alt="mail icon"
+//                 />
+//                 <p>{email}</p>
+//             </div>
+//         </article>
+//     )
+// }
