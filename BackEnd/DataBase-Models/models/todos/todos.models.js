b0cdbcd298d6d385a97b8dea6343e3634eec4f6e
akshad-3 <akshadofficial3@gmail.com>
2026-05-20 21:20:20 +0530

    creating new DataBase model in MongoDB using Mongoose

---
+++ BackEnd/DataBase-Models/models/todos/todos.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+
+const todoSchema = new mongoose.Schema({
+  content: {
+    type : String,
+    required : true
+  },
+  complete: {
+    type: Boolean,
+    default: false
+  },
+  createdBy : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : "user"
+  },
+  sudTodos: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'SubTodo'
+    },
+  ],
+},{timestamps:true})
+
+export const todo = mongoose.model("Todo", todoSchema)
